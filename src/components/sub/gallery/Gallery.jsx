13496d98baaa8888643b82a0f2a8607b9c4fe069
YOUNTAEHEE <f_hyang@naver.com>
2023-12-07 10:24:06 +0900

    gallery

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,12 +1,63 @@
 import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
-
+import Masonry from 'react-masonry-component';
+import { useCustomText } from '../../../hooks/useText';
+import { LuSearch } from 'react-icons/lu';
 export default function Gallery() {
 	const myID = useRef('199625511@N07');
-	const [Pics, setPics] = useState([]);
-const refNav = useRef(null);
+	const isUser = useRef(myID.current);
 
+	const gap = useRef(20);
+	const refNav = useRef(null);
+
+	const refFrmaeWrap = useRef(null);
+	const [Pics, setPics] = useState([]);
+	const shortenText = useCustomText('shorten');
+
+	const activateBtn = (e) => {
+		const btns = refNav.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e && e.target.classList.add('on');
+	};
+
+	const handleInterest = (e) => {
+		if (e.target.classList.contains('on')) return;
+		isUser.current = '';
+		activateBtn(e);
+		fetchFlickr({ type: 'interest' });
+	};
+
+	const handleMine = (e) => {
+		console.log('btn');
+		console.log('myID', myID.current);
+		console.log('isUser', isUser.current);
+		if (e.target.classList.contains('on') || isUser.current === myID.current) return;
+		isUser.current = myID.current;
+		activateBtn(e);
+		fetchFlickr({ type: 'user', id: myID.current });
+	};
+	const handleUser = (e) => {
+		console.log('handleMine called');
+		console.log('isUser.current:', isUser.current);
+		console.log('myID.current:', myID.current);
+		if (isUser.current) return;
+		isUser.current = e.target.innerText;
+		activateBtn();
+		fetchFlickr({ type: 'user', id: e.target.innerText });
+	};
+	const handleSearch = (e) => {
+		console.log('handleMine called');
+		console.log('isUser.current:', isUser.current);
+		console.log('myID.current:', myID.current);
+		e.preventDefault();
+		isUser.current = '';
+		activateBtn();
+		const keyword = e.target.children[0].value;
+		if (!keyword.trim()) return;
+		e.target.children[0].value = '';
+		fetchFlickr({ type: 'search', keyword: keyword });
+	};
 	const fetchFlickr = async (opt) => {
 		const num = 50;
 		const flickr_api = process.env.REACT_APP_FLICKR_API;
@@ -28,22 +79,53 @@ const refNav = useRef(null);
 		setPics(json.photos.photo);
 	};
 	useEffect(() => {
-		fetchFlickr({ type: 'interest' });
+		refFrmaeWrap.current.style.setProperty('--gap', gap.current + 'px');
+		fetchFlickr({ type: 'user', id: myID.current });
 	}, []);
-	return <Layout title={'Gallery'}>
-		<article className='controls'>
-			<nav className='btnSet' ref={refNav}>
-				<button>Interest Gallery</button>
-				<button>My Gallery</button>
-			</nav>
+	return (
+		<Layout title={'Gallery'}>
+			<article className='controls'>
+				<nav className='btnSet' ref={refNav}>
+					<button onClick={handleInterest}>Interest Gallery</button>
+					<button className='on' onClick={handleMine}>
+						My Gallery
+					</button>
+				</nav>
 
-			<form>
-				<input type="text" placeholder='Search' />
+				<form onSubmit={handleSearch}>
+					<input type='text' placeholder='Search' />
 
-				<button></button>
-			</form>
+					<button className='btnSearch'>
+						<LuSearch />
+					</button>
+				</form>
+			</article>
+			<section className='frameWrap' ref={refFrmaeWrap}>
+				<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: gap.current }}>
+					{Pics.length === 0 ? (
+						<h2>해당 키워드에 대한 검색 결과가 없습니다.</h2>
+					) : (
+						Pics.map((pic, idx) => {
+							return (
+								<article key={pic.id}>
+									<div className='pic'>
+										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+									</div>
 
-
-		</article>
-	</Layout>;
+									<div className='txt'>
+										<i onClick={handleUser}>{pic.owner}</i>
+										<h2>{shortenText(pic.title, 20)}</h2>
+										<div className='viewBox'>
+											<p className='line'></p>
+											<p>view</p>
+										</div>
+									</div>
+								</article>
+							);
+						})
+					)}
+				</Masonry>
+			</section>
+		</Layout>
+	);
 }
