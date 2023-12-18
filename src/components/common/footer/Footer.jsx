3d08378f6e5c8a5028b9ec28ed310ba9618c5428
YOUNTAEHEE <f_hyang@naver.com>
2023-12-18 20:13:50 +0900

    footer url

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,28 +1,43 @@
-import './Footer.scss';
-import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
+import "./Footer.scss";
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
 export default function Footer() {
-	return (
-		<footer className='Footer'>
-			<div className='footerLeft'>
-				<h1>YOUN</h1>
-				<p>175 Varrick Street, 3rd Floor. New York, NY 10014</p>
-				<p>github.com/YOUNTAHEE | 010-1234-5678</p>
-			</div>
-			<div className='footerRight'>
-				<p>2023 YOUN &copy; All Rights Reserved.</p>
+  const facebookURL = "https://www.facebook.com/";
+  const twitterURL = "https://twitter.com/";
+  const youtubeURL = "https://www.youtube.com/";
+  return (
+    <footer className="Footer">
+      <div className="footerLeft">
+        <h1>YOUN</h1>
+        <p>175 Varrick Street, 3rd Floor. New York, NY 10014</p>
+        <p>github.com/YOUNTAHEE | 010-1234-5678</p>
+      </div>
+      <div className="footerRight">
+        <p>2023 YOUN &copy; All Rights Reserved.</p>
 
-					<ul>
-						<li>
-							<FaFacebookF />
-						</li>
-						<li>
-							<FaTwitter />
-						</li>
-						<li>
-							<FaYoutube />
-						</li>
-					</ul>
-			</div>
-		</footer>
-	);
+        <ul>
+          <li
+            onClick={() => {
+              window.open(facebookURL);
+            }}
+          >
+            <FaFacebookF />
+          </li>
+          <li
+            onClick={() => {
+              window.open(twitterURL);
+            }}
+          >
+            <FaTwitter />
+          </li>
+          <li
+            onClick={() => {
+              window.open(youtubeURL);
+            }}
+          >
+            <FaYoutube />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
