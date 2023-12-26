3803c280f20d79ecc38658b0929c4bb6ecd522c1
YOUNTAEHEE <f_hyang@naver.com>
2023-12-26 09:29:49 +0900

    main

--- src/components/main/intro/Intro.jsx
+++ src/components/main/intro/Intro.jsx
@@ -4,45 +4,55 @@ import { useSelector } from "react-redux";
 export default function Intro() {
   const memberReducer = useSelector((store) => store.memberReducer);
   const MemberData = Object.values(memberReducer.member)[0];
-
+  console.log(MemberData);
   return (
     <section className="Intro">
-      <h2>Who We Are?</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis,
-        cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Aperiam ad cumque error molestias ipsa saepe sequi sunt explicabo dicta
-        totam.
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eius?
-      </p>
-      <article>
-        {MemberData.map((data, idx) => {
+      <div className="txtBox">
+        <h2>
+          Who <strong>We Are?</strong>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis,
+          cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Aperiam ad cumque error molestias ipsa saepe sequi sunt explicabo
+          dicta totam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, architecto. Nam corrupti numquam repudiandae animi quis quod obcaecati esse, ad doloribus qui amet iste similique officia nesciunt magni velit voluptatibus!
+          Dolores explicabo laboriosam, ab aperiam minima voluptatibus reprehenderit impedit suscipit? Minima, dicta exercitationem nobis voluptas laboriosam, obcaecati neque, ab ea repellendus excepturi dolorum? Dignissimos sed, inventore obcaecati similique voluptas facere.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab architecto adipisci nesciunt natus doloribus minima similique maxime nemo aut! Nostrum veritatis natus, provident doloribus asperiores ducimus nihil sapiente officiis omnis?
+        </p>
+        <button>MORE ABOUT COMPANY</button>
+      </div>
+      <div className="memberPicWrap">
+        {MemberData?.map((data, idx) => {
           if (idx >= 1) return null;
           return (
-            <div className="mainIntroPic" key={data + idx}>
-              <img
-                src={`${process.env.PUBLIC_URL}/img/${data.pic}`}
-                alt={data.name}
-              />
-            </div>
+            <article key={data + idx}>
+              <div className="mainIntroPicBg">
+                <img
+                  src={`${process.env.PUBLIC_URL}/img/${data.pic}`}
+                  alt={data.name}
+                />
+              </div>
+            </article>
           );
         })}
-      </article>
-      <article>
-        {MemberData.map((data, idx) => {
+
+        {MemberData?.map((data, idx) => {
           if (idx >= 1) return null;
           return (
-            <div className="mainIntroPicBg" key={data + idx}>
-              <img
-                src={`${process.env.PUBLIC_URL}/img/${data.pic}`}
-                alt={data.name}
-              />
-            </div>
+            <article key={data + idx}>
+              <div className="mainIntroPicBg">
+                <img
+                  src={`${process.env.PUBLIC_URL}/img/${data.pic}`}
+                  alt={data.name}
+                />
+              </div>
+            </article>
           );
         })}
-      </article>
+      </div>
     </section>
   );
 }
