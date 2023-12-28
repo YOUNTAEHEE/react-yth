4467f69c638417d12ba34d03130229eb1eab9f24
YOUNTAEHEE <f_hyang@naver.com>
2023-12-28 21:37:13 +0900

    main add

--- src/components/main/intro/Intro.jsx
+++ src/components/main/intro/Intro.jsx
@@ -1,6 +1,5 @@
 import { useMemberQuery } from "../../../hooks/useMemberQuery";
 import "./Intro.scss";
-import { useSelector } from "react-redux";
 
 export default function Intro() {
   const { data: MemberData, isSuccess: isMember } = useMemberQuery();
@@ -14,29 +13,39 @@ export default function Intro() {
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis,
           cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           Aperiam ad cumque error molestias ipsa saepe sequi sunt explicabo
-          dicta totam.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, architecto. Nam corrupti numquam repudiandae animi quis quod obcaecati esse, ad doloribus qui amet iste similique officia nesciunt magni velit voluptatibus!
-          Dolores explicabo laboriosam, ab aperiam minima voluptatibus reprehenderit impedit suscipit? Minima, dicta exercitationem nobis voluptas laboriosam, obcaecati neque, ab ea repellendus excepturi dolorum? Dignissimos sed, inventore obcaecati similique voluptas facere.
+          dicta totam. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Voluptatem, architecto. Nam corrupti numquam repudiandae animi quis
+          quod obcaecati esse, ad doloribus qui amet iste similique officia
+          nesciunt magni velit voluptatibus! Dolores explicabo laboriosam, ab
+          aperiam minima voluptatibus reprehenderit impedit suscipit? Minima,
+          dicta exercitationem nobis voluptas laboriosam, obcaecati neque, ab ea
+          repellendus excepturi dolorum? Dignissimos sed, inventore obcaecati
+          similique voluptas facere.
           <br />
           <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab architecto adipisci nesciunt natus doloribus minima similique maxime nemo aut! Nostrum veritatis natus, provident doloribus asperiores ducimus nihil sapiente officiis omnis?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+          eius?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+          architecto adipisci nesciunt natus doloribus minima similique maxime
+          nemo aut! Nostrum veritatis natus, provident doloribus asperiores
+          ducimus nihil sapiente officiis omnis?
         </p>
         <button>MORE ABOUT COMPANY</button>
       </div>
       <div className="memberPicWrap">
-        {isMember && MemberData.map((data, idx) => {
-          if (idx >= 1) return null;
-          return (
-            <article key={data + idx}>
-              <div className="mainIntroPicBg">
-                <img
-                  src={`${process.env.PUBLIC_URL}/img/${data.pic}`}
-                  alt={data.name}
-                />
-              </div>
-            </article>
-          );
-        })}
+        {isMember &&
+          MemberData.map((data, idx) => {
+            if (idx >= 1) return null;
+            return (
+              <article key={data + idx}>
+                <div className="mainIntroPicBg">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/img/${data.pic}`}
+                    alt={data.name}
+                  />
+                </div>
+              </article>
+            );
+          })}
 
         {MemberData?.map((data, idx) => {
           if (idx >= 1) return null;
