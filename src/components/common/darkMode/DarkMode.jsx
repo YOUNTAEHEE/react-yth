16cc2e58f116f852bb5b7807f0d0deb2c99162fd
YOUNTAEHEE <f_hyang@naver.com>
2023-12-29 09:27:20 +0900

    add

---
+++ src/components/common/darkMode/DarkMode.jsx
@@ -0,0 +1,9 @@
+import "./DarkMode.scss";
+import { useCookie } from "../../../hooks/useCookie";
+import { useGlobalData } from "../../../hooks/useGlobalData";
+export default function DarkMode() {
+  const { setCookie, isCookie } = useCookie();
+  const { Mode, setMode } = useGlobalData();
+
+  return <div className={``}>DarkMode</div>;
+}
